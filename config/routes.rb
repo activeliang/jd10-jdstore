e60e61b42108c9b5a90e69b435f21dbbaee9ac6e
activeliang <activeliang@gmail.com>
2017-03-25 20:15:33 +0800

    implement ordermailer function

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
   end
 
   resources :products do
@@ -23,5 +31,15 @@ Rails.application.routes.draw do
 
   resources :cart_items
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_wechat
+      post :pay_with_alipay
+      post :apply_to_cancel
+    end
+  end
+
+  namespace :account do
+    resources :orders
+  end
 end
